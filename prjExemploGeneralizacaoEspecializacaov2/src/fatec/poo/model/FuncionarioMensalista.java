8542b46a70ef7b054c3e35cb8815dfe3f9dd44ad
cassiohirota <41903845+cassiohirota@users.noreply.github.com>
2022-04-05 10:50:51 -0300

    Add files via upload

---
+++ prjExemploGeneralizacaoEspecializacaov2/src/fatec/poo/model/FuncionarioMensalista.java
@@ -0,0 +1,22 @@
+package fatec.poo.model;
+
+
+public class FuncionarioMensalista extends Funcionario{
+    private double valSalMin;
+    private double numSalMin;
+
+    public FuncionarioMensalista(int registro, String nome, String dtAdmissao, double valSalMin) {
+        super(registro, nome, dtAdmissao);
+        this.valSalMin = valSalMin;
+    }
+
+    public void setNumSalMin(double numSalMin) {
+        this.numSalMin = numSalMin;
+    }
+
+    @Override
+    public double calcSalBruto() {
+        return (valSalMin * numSalMin);
+    }
+
+}
