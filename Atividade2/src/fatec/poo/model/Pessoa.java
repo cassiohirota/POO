b2772c0381f696db90b5b257d7e301581e3575f3
cassiohirota <41903845+cassiohirota@users.noreply.github.com>
2022-04-11 09:21:07 -0300

    Add files via upload

---
+++ Atividade2/src/fatec/poo/model/Pessoa.java
@@ -0,0 +1,32 @@
+package fatec.poo.model;
+
+public abstract class Pessoa {
+    
+    private String nome;
+    private int anoInscricao;
+    private double totalCompras;
+
+    public Pessoa(String nome, int anoInscricao) {
+        this.nome = nome;
+        this.anoInscricao = anoInscricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoInscricao() {
+        return anoInscricao;
+    }
+
+    public double getTotalCompras() {
+        return totalCompras;
+    }
+        
+    public void addCompras(double valor){
+        totalCompras += valor;
+    }
+
+    public abstract double calcBonus(int anoAtual);
+
+}
