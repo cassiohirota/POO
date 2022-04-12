870023243fbb8359817281146c7d5b9c315e758a
cassiohirota <41903845+cassiohirota@users.noreply.github.com>
2022-04-12 11:00:54 -0300

    Add files via upload

---
+++ atividade4/src/Aplic.java
@@ -0,0 +1,12 @@
+
+import fatec.poo.model.Cliente;
+import fatec.poo.model.Instrutor;
+
+public class Aplic {
+
+    public static void main(String[] args) {
+        Instrutor pesInstr = new Instrutor(12345, "Joao", "23435435");
+        Cliente pesCli = new Cliente("232435435234", "Felipe", "43245354");
+    }
+    
+}
