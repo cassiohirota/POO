fe4369a57429d2ef9383e257ead32b5658b94907
cassiohirota <41903845+cassiohirota@users.noreply.github.com>
2022-05-02 09:24:53 -0300

    Add files via upload

---
+++ prjExemploAssociacaoBinaria/src/fatec/poo/model/Funcionario.java
@@ -0,0 +1,62 @@
+package fatec.poo.model;
+
+/**
+ *
+ * @author dimas
+ */
+public abstract class Funcionario {
+    private int registro;
+    private String nome;
+    private String dtAdmissao;
+    private String cargo;
+    private Departamento departamento; //Implementação da multiplicidade 1
+    
+        
+    public Funcionario(int registro, String nome, String dtAdmissao) {
+        this.registro = registro;
+        this.nome = nome;
+        this.dtAdmissao = dtAdmissao;
+    }
+    
+    //Definição de um método abstrato
+    abstract public double calcSalBruto();
+    
+    public double calcDesconto(){
+        return(0.10 * calcSalBruto());
+    }
+    
+    public double calcSalLiquido(){
+        return(calcSalBruto() - calcDesconto());
+    }
+
+    public int getRegistro() {
+        return registro;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDtAdmissao() {
+        return dtAdmissao;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    //Retorna o endereço de um objeto da classe Departamento
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    //Tem como parametro de entrada o endereço de um objeto da classe departamento
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+    
+}
